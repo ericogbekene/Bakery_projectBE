0171f12f106b4490d832f01c68d8c3561cbaa4c1
Eric Ogbekene <ericogbekene@icloud.com>
2024-10-23 13:08:01 -0700

    Feat/addding requirements dot txt

--- bake_world/products/urls.py
+++ bake_world/products/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from .views import ProductViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+# router.register('users', UserViewSet, 'user')
+router.register(r'products', ProductViewSet, basename='products')
+
+
 
 urlpatterns = [
-    path('products/', ProductViewSet.as_view(), name='products'),
-    path('jobs/', views.jobs, name='jobs')
+    path('', include(router.urls))
+    #path('products/', ProductViewSet.as_view(), name='products'),
+    #path('jobs/', views.jobs, name='jobs')
 ]
