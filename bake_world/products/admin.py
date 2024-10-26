ef22312ef63c2bb09e22cb5ec1761894837493fd
cysof <cp4reallife@yahoo.com>
2024-10-26 15:12:48 +0100

    admin updated

--- bake_world/products/admin.py
+++ bake_world/products/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 
-from .models import Product, Category, Order
+from .models import Product, Category, Order, OrderItem, Cart, CartItem
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin): 
+    inlines = [OrderItemInline]
+    list_display = ('id', 'user', 'quantity', 'created_at', 'updated_at')
+
+class CartAdmin(admin.ModelAdmin):
+    inlines = [CartItemInline]
+    list_display = ('id', 'user', 'created_at', 'updated_at')
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Cart, CartAdmin)
+admin.site.register(CartItem)
+
 
 admin.site.register(Product)
 admin.site.register(Category)
