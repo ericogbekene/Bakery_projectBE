3ac576b8be0bb3d06361bf111dd3c0d469c6e12c
cysof <cp4reallife@yahoo.com>
2025-03-14 12:03:22 +0100

    update: update product category display

--- products/serializers.py
+++ products/serializers.py
@@ -5,10 +5,11 @@ from rest_framework import serializers
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ["id", "name", "slug"]
+        fields = ["name"]
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
     class Meta:
         model = Product
         fields = [
